8176c5142cff9db9e77a9516422c13813a63fa8a
Nestor Waldyd <nwaldyd@yahoo.co.uk>
2013-02-16 22:01:45 -0500

    version 0

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fileupload::Application.config.secret_token = 'a1e7975fd5f8bdd382982dc8072f2ba57174f9ad9ead6a58e1160b660643ea69c3c1944cb9218491c11816d7f307e64af7ef178cd8ee7ad6bb241aa663756035'
